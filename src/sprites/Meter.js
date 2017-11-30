e3931869eed027ed028f02aaab492f5dfe290606
Sam Barker <samuel.e.barker@googlemail.com>
2017-11-30 22:11:01 +0000

    added thumbnail gif and charge sound

--- src/sprites/Meter.js
+++ src/sprites/Meter.js
@@ -17,6 +17,8 @@ export default class extends Phaser.Sprite {
 
         this.boomerang = boomerang;
         this.player = player;
+        this.chargeSound = game.add.audio('chargeMeter')
+        this.chargePlayed = false
     }
 
     powerUp () {
@@ -26,12 +28,18 @@ export default class extends Phaser.Sprite {
 
     update() {
         if (this.throwKey.isDown) {
+            if (!this.chargePlayed) {
+                this.chargeSound.play()
+                this.chargePlayed = true
+            }
             this.powerUp();
             this.alpha = 1;
             this.position.y = this.player.position.y - (1 + (this.sPower * this.sPower) * 410)
             this.position.x = this.player.position.x
             game.input.keyboard.onUpCallback = function (e) {
                 if (e.keyCode === this.throwKey.keyCode) {
+                    this.chargePlayed = false
+                    this.chargeSound.pause()
                     game.add.tween(this).to( { alpha: 0 }, 500, Phaser.Easing.Linear.None, true, 0, 0, false);
                     this.boomerang.launch(this.sPower);
                     this.power = this.maxPower / 2;
